6b23f460210343ebe1fd9288ee8f0408a2f3ec5b
Emma O'Neill <emmaruthoneill@gmail.com>
2018-05-17 14:25:58 -0700

    trying to get modules to work

--- src/js/main.js
+++ src/js/main.js
@@ -34,26 +34,26 @@ var localDataURL = "http://extras.sfgate.com/editorial/election2018primary/live/
 // ca_props_lib.CAPropsLib;
 
 // this is where I was trying to call the function to put in state propositions data in the boxes
-// var ca_props_lib_boxes = require("./CAprops_boxes.js");
-// d3.json(propsCAURL,ca_props_lib_boxes.CAPropsBoxes(prosCA));
+var ca_props_lib_boxes = require("./CAprops_boxes.js");
+d3.json(propsCAURL,ca_props_lib_boxes.CAPropsBoxes(propsCA));
 
 // THIS IS THE FUNCTION I WAS TRYING TO CALL FROM "CAprops_boxes.js":
-d3.json(propsCAURL, function(propsCA){
-  for (var propidx=68; propidx<73; propidx++) {
-    var propID = document.getElementById("prop"+propidx);
-    // FIX THIS WHEN WE HAVE REAL DATA NOT FAKE DATA!-----------------------------------------------
-    var propResult = propsCA[propidx-5]["state"];
-    // FIX THIS WHEN WE HAVE REAL DATA NOT FAKE DATA!-----------------------------------------------
-    var total = +propResult.r["Yes"]+ +propResult.r["No"];
-    if (total == 0) { total = 0.1;}
-    if (propResult.d == "Yes") {
-      var htmlresult = "<span class='propyes yesresult'><i class='fa fa-check-circle-o' aria-hidden='true'></i>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno'>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
-    } else if (propResult.d == "No") {
-      var htmlresult = "<span class='propyes'>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno noresult'><i class='fa fa-times-circle-o' aria-hidden='true'></i>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
-    } else {
-      var htmlresult = "<span class='propyes'>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno'>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
-    }
-    var htmlresult = htmlresult+ "<div class='prop-precincts'>"+formatthousands(propResult.p)+" / 24,849 precincts reporting</div>"
-    propID.innerHTML = htmlresult;
-  }
-});
+// d3.json(propsCAURL, function(propsCA){
+//   for (var propidx=68; propidx<73; propidx++) {
+//     var propID = document.getElementById("prop"+propidx);
+//     // FIX THIS WHEN WE HAVE REAL DATA NOT FAKE DATA!-----------------------------------------------
+//     var propResult = propsCA[propidx-5]["state"];
+//     // FIX THIS WHEN WE HAVE REAL DATA NOT FAKE DATA!-----------------------------------------------
+//     var total = +propResult.r["Yes"]+ +propResult.r["No"];
+//     if (total == 0) { total = 0.1;}
+//     if (propResult.d == "Yes") {
+//       var htmlresult = "<span class='propyes yesresult'><i class='fa fa-check-circle-o' aria-hidden='true'></i>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno'>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
+//     } else if (propResult.d == "No") {
+//       var htmlresult = "<span class='propyes'>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno noresult'><i class='fa fa-times-circle-o' aria-hidden='true'></i>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
+//     } else {
+//       var htmlresult = "<span class='propyes'>Yes: "+Math.round(propResult.r["Yes"]/total*1000)/10+"%</span><span class='propno'>No: "+Math.round(propResult.r["No"]/total*1000)/10+"%</span>"
+//     }
+//     var htmlresult = htmlresult+ "<div class='prop-precincts'>"+formatthousands(propResult.p)+" / 24,849 precincts reporting</div>"
+//     propID.innerHTML = htmlresult;
+//   }
+// });
